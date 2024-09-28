dcb7d1f128e31c5984769c35964aa2733387a01e
MasterNebula <master.nebula2255@gmail.com>
2024-09-29 00:59:32 +0530

    hello_time.php

--- time.php
+++ time.php
@@ -1,5 +1,5 @@
 <?php
 date_default_timezone_set('Asia/Kolkata');
 $current_time = date('Y-m-d H:i:s');
-echo "Hello Alika C, right now the time is $current_time";
+echo "Hello Adithyan Shabu, right now the time is $current_time";
 ?>
