c6254671bef3dfcc52483b53acfe519eb65d342a
MasterNebula <master.nebula2255@gmail.com>
2024-09-29 00:57:58 +0530

    main.rs

--- main.rs
+++ main.rs
@@ -2,5 +2,5 @@ use chrono::Local;
 
 fn main(){
 	let current_time = Local::now();
-	println!("Hello Alika C, right now the time is {}", current_time.format("%Y-%m-%d %H:%M:%S"));
+	println!("Hello Adithyan Shabu, right now the time is {}", current_time.format("%Y-%m-%d %H:%M:%S"));
 }
